ee9efaed41c973bb73787266405da2951bb6c53a
Shashidhar Angadi <shashidharangadi14@gmail.com>
2024-07-30 18:48:37 +0530

    Addition of Context API, Food menu and display section

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom'
+import StoreContextProvider from './context/storeContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+    <StoreContextProvider>
       <App />  {/*By wrapping the app component into browserRouter it enables the client-side routing */}
+    </StoreContextProvider>
   </BrowserRouter>
     
 )
